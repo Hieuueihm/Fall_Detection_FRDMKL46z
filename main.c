cfaa0737ef8c4c1e555469c72cf08dc2aec5de95
abc <gbmhieu123@gmail.com>
2024-06-13 10:04:10 +0700

    push

--- main.c
+++ main.c
@@ -131,7 +131,7 @@ void PORTC_PORTD_IRQHandler()
       case STATE_1:
         state = STATE_2;
         // uart_send_msg("state1");
-        accel_config(MOTION_DETECTION, 40, 5);
+        accel_config(MOTION_DETECTION, 40, 3);
         break;
       case STATE_2:
         // uart_send_msg("state2");
